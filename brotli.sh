b7ee5bf14aab0a2e01b8a0ebcc6e432585a03b2f
Amy Jie <git@xvrqt.com>
2020-05-25 15:53:53 -0700

    Moved icons to the top level or src/ and removed the brotli script

--- brotli.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-OLDIFS=$IFS
-IFS=$'\n'
-for FILE in $(find . -type f -iname '*.html' -o -iname '*.css' -o -iname '*.js' -o -iname '*.svg' -o -iname '*.json'); do
-    echo -n "Compressing ${FILE}..."
-    brotli ${FILE} -f -q 11
-    echo "done."
-done
-IFS=$OLDIFS
